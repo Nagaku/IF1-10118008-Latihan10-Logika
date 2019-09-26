5b746290739ada81ad238ded6e480a70e6589bc9
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-09-26 18:42:30 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan10-Logika/src/latihan10/Latihan10.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan10;
+
+/**
+ *
+ *  @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF1
+ * NIM			: 10118008
+ * Deskripsi Program	: Program ini berisi program untuk menampilkan biodata rockstar
+ */
+public class Latihan10 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		int umur = 19;
+		boolean dewasa;
+		dewasa = (umur >= 17);
+		System.out.println("Status Dewasa : " + dewasa);
+	}
+	
+}
